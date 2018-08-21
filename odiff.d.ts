822b764c1df19fb048b1e0835ecb3f82b2f4365d
Christophe Lemonnier <clemonnier@sync.tv>
2018-08-21 22:57:21 +0200

    added typescript definition

---
+++ odiff.d.ts
@@ -0,0 +1,17 @@
+interface odiffResult {
+    type: "set" | "unset" | "add" | "rm";
+    path: (string | number)[];
+    val: any;
+    index: number;
+    vals: any[];
+    num: number;
+}
+
+interface odiff {
+    equal(a: any, b: any): boolean;
+    similar(a: any, b: any): boolean;
+}
+
+declare function odiff(a: any, b: any): odiffResult[];
+
+export default odiff;
